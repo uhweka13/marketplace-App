52eb4c715ba1863040719148b6c0de47d81dc746
uhweeka ika <uhweka@gmail.com>
2020-03-05 09:31:30 +0100

    dashboard and add shop started

--- app/src/main/java/com/example/marketplace/fragmentUi/MainDashboardFragment.kt
+++ app/src/main/java/com/example/marketplace/fragmentUi/MainDashboardFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 import com.example.marketplace.R
+import kotlinx.android.synthetic.main.fragment_main_dashboard.*
+import kotlinx.android.synthetic.main.fragment_main_dashboard.view.*
+
 /**
  * A simple [Fragment] subclass.
  */
 class MainDashboardFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    lateinit var main_view:View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_dashboard, container, false)
+        main_view = inflater.inflate(R.layout.fragment_main_dashboard, container, false)
+
+        val spinner: Spinner = main_view.sp_category
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        return main_view
     }
 
 }
--- app/src/main/java/com/example/marketplace/fragmentUi/MessageFragment.kt
+++ app/src/main/java/com/example/marketplace/fragmentUi/MessageFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.marketplace.R
+
 /**
  * A simple [Fragment] subclass.
  */
 class MessageFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_message, container, false)
     }
